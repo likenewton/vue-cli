437dad493bc64fe8a34786ab37bc2071c03a9ffe
likenewton <52875440@qq.com>
2018-11-27 00:05:48 +0800

    这是我在家里的macbookpro上提交的内容

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
   </div>
 </template>
 
@@ -10,15 +9,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'This is content'
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+
 </style>
