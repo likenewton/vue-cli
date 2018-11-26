aa72b099ef88705db6b1397cd14631b21b55a524
Newton <52875440@qq.com>
2018-11-26 18:44:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="page-container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import vHeader from '@/components/common/vHeader'
+Vue.component('vHeader', vHeader)
+
+export default {
+  name: 'App'
+}
+
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
---
+++ src/components/common/vHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="v-header">
+    <ul class="nav-list">
+      <li v-for="(item, index) in list" :key="index">
+        {{item.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'vHeader',
+  data () {
+    return {
+      list: [{
+        text: '首页'
+      }, {
+        text: '游戏产品'
+      }, {
+        text: '客服中心'
+      }, {
+        text: '关于我们'
+      }, {
+        text: '用户协议'
+      }]
+    }
+  }
+}
+
+</script>
+<style scoped>
+</style>
